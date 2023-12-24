dbb44bbe7688830fd05c68f9a3bb5c89bd37ee52
Hil2O <loveheartassault@gmail.com>
2023-12-24 20:28:36 +0900

    Initialize new Maven project with Spring Boot
    This commit sets up a new Maven project with a Spring Boot parent. The project includes the necessary Maven wrapper files, properties files, gitignore, and Java application files. It also initializes a basic unit test for the application.

---
+++ src/main/java/com/example/rensyu03/Rensyu03Application.java
@@ -0,0 +1,13 @@
+package com.example.rensyu03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rensyu03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rensyu03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rensyu03/Rensyu03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rensyu03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rensyu03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
